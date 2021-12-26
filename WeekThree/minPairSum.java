397762749c7ecdf3a42efe70323375b19368472f
AndualemSebsbe <andualemsebsbe1@gmail.com>
2021-12-27 02:29:10 +0300

    Solved Minimize Maximum Pair Sum in Array

---
+++ WeekThree/minPairSum.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minPairSum(int[] nums) {
+      int len = nums.length;
+      Arrays.sort(nums);
+        int l=0;// the first index of the array
+        int r=len-1;// the last index of the array
+        int max=0, sumOfPair=0;
+      for(int i=0; i<len/2; i++){
+          sumOfPair = nums[l++] + nums[r--];
+          max = Math.max(sumOfPair,max);
+      }
+       
+      return max;
+    }
+}
