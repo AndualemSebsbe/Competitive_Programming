7c20c86ee0221b8f8c61d0249231ffbad427c57e
AndualemSebsbe <andualemsebsbe1@gmail.com>
2022-03-06 23:32:16 +0300

    Camp Contest Two Solutions

---
+++ CampContest/newYearAndHurry.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class newYearAndHurry{
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String[] str = s.split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        int num = k;
+        int count=0, i=1; 
+        while((count < n)&&(num <= 240)){
+            
+        if(((num += (5*i)) > 240)) break;
+            i++; count++;
+        }
+        System.out.println(count);
+    }
+}
