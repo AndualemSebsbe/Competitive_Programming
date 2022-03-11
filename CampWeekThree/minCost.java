8debdd6ff5d952b3e37d23118e80b3797c36565e
AndualemSebsbe <andualemsebsbe1@gmail.com>
2022-03-11 03:38:04 +0300

    Solved Min Cost Climbing Stairs

---
+++ CampWeekThree/minCost.java
@@ -0,0 +1,8 @@
+class minCost {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        for(int i = 2; i < len; i++)
+            cost[i] += Math.min(cost[i - 1], cost[i -2]);
+        return Math.min(cost[len - 1], cost[len - 2]);
+    }
+}
