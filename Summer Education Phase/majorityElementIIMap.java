01603d4dc881064670cadbc3d9f4687d77cbb372
AndualemSebsbe <andualemsebsbe1@gmail.com>
2022-09-01 14:28:29 +0300

    Solved Majarity Element II Using HashMap

---
+++ Summer Education Phase/majorityElementIIMap.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class majorityElementIIMap {
+
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap();
+        for(int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+        List<Integer> list = new ArrayList();
+        for(int key : map.keySet()){
+            if(map.get(key) > n/3)
+                list.add(key);
+        }
+        
+        return list;
+    }
+}
