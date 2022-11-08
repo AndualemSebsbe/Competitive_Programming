5bbee6644b1076113ca2f22446369995d12088cb
Andualem <94195392+AndualemSebsbe@users.noreply.github.com>
2022-11-08 23:10:46 +0300

    Time: 6 ms (66.24%), Space: 43.8 MB (68.26%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            int sum = 0;
+            for(int j = 0; j < n; j++){
+                if(((nums[j] >> i) & 1) == 1){
+                    sum += 1;
+                    sum %= 3;
+                }
+            }
+            
+            if(sum != 0)
+                ans |= sum << i;
+        }
+        
+        return ans;
+    }
+}
