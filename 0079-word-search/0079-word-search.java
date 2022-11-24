910e5a20a38c541f11d99be350b6e31303a3a1f6
Andualem <94195392+AndualemSebsbe@users.noreply.github.com>
2022-11-24 16:54:45 +0300

    Time: 472 ms (9.23%), Space: 144.8 MB (5.33%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,40 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        int len = word.length();
         int m = board.length;
         int n = board[0].length;
-        int res = 0;
-        
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if(board[i][j] == word.charAt(0) && searchWord(board, i, j, word, 0))
+       
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                char c = word.charAt(0);
+                if(board[i][j] == c && wordSearch(board, i, j, word, 0))
                     return true;
-
+            }
+        }
+        
         return false;
     }
     
-    public boolean searchWord(char[][] board, int i, int j, String word, int idx){
+    public boolean wordSearch(char[][] board, int r, int c, String word, int idx){
         if(idx == word.length())
             return true;
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(idx))
+        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || 
+           board[r][c] != word.charAt(idx))
             return false;
         
-        char temp = board[i][j];
-        board[i][j] = ' ';
+        char temp = board[r][c];
+        board[r][c] = ' ';
+        int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        boolean found = false;
+        
+        for(int[] dir : dirs){
+            int newR = r + dir[0];
+            int newC = c + dir[1];
             
-        boolean found = searchWord(board, i+1, j, word, idx+1) ||
-                        searchWord(board, i-1, j, word, idx+1) ||
-                        searchWord(board, i, j+1, word, idx+1) ||
-                        searchWord(board, i, j-1, word, idx+1);
-        board[i][j] = temp; 
+           found = found || wordSearch(board, newR, newC, word, idx+1);
+        }
+        board[r][c] = temp;
+        
         return found;
     }
 }
