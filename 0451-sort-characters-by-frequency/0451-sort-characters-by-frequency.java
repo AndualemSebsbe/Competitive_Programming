bdf4791daa9327dbe55a1efe3d44387af6650133
Andualem <94195392+AndualemSebsbe@users.noreply.github.com>
2022-12-03 17:04:51 +0300

    Time: 18 ms (85.29%), Space: 42.8 MB (96.83%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -4,28 +4,17 @@ class Solution {
         for(int i = 0; i < s.length(); i++)
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         
-        List<Integer> list = new ArrayList();
-        for(char key : map.keySet())
-            if(!list.contains(map.get(key)))
-                list.add(map.get(key));
+        PriorityQueue<Character> maxHeap = new PriorityQueue((a,b) -> map.get(b)-map.get(a));
+        maxHeap.addAll(map.keySet());
         
-        Collections.sort(list);
-        Collections.reverse(list);
-        
-        Map<Integer, List<Character>> listMap = new HashMap();
-        for(char key : map.keySet()){
-            if(!listMap.containsKey(map.get(key)))
-                listMap.put(map.get(key), new ArrayList());
-            listMap.get(map.get(key)).add(key);
+        StringBuilder sb = new StringBuilder();
+        while(!maxHeap.isEmpty()){
+            char curr = maxHeap.poll();
+            
+            for(int i = 0; i < map.get(curr); i++)
+                sb.append(curr);
         }
         
-        String res = "";
-        for(int i = 0; i < list.size(); i++){
-            for(char c : listMap.get(list.get(i)))
-                for(int j = 0; j < list.get(i); j++)
-                    res += c;
-        }
-        
-        return res;
+        return sb.toString();
     }
 }
