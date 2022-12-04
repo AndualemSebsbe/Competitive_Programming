2cc204469c3a86e96951cf56a6240190b5d8dc9b
Andualem <94195392+AndualemSebsbe@users.noreply.github.com>
2022-12-05 00:44:08 +0300

    Time: 3 ms (94.55%), Space: 57.1 MB (98.04%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
-        long[] prefixSum = new long[n];
-        prefixSum[0] = nums[0];
+        long total = 0;
         
-        for(int i = 1; i < n; i++)
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+        for(int i = 0; i < n; i++)
+            total += nums[i];
         
         int count = 0;
+        long leftSum = 0;
         for(int i = 0; i < n-1; i++){
-            long leftSum = prefixSum[i];
-            long rightSum = prefixSum[n-1] - leftSum;
+            leftSum += nums[i];
+            long rightSum = total - leftSum;
             
             if(leftSum >= rightSum)
                 count += 1;
@@ -19,3 +19,4 @@ class Solution {
         return count;
     }
 }
+//SC : O(1)
