3c3c67104f5c7d3457e747b38f534f82de4777a9
AndualemSebsbe <andualemsebsbe1@gmail.com>
2023-02-27 23:49:47 +0300

    Solved Copy List with Random Pointer

---
+++ A2SV Squid Game Round4/copyListWithRandomPointer.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class copyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        map.put(null, null);
+        while(cur != null){
+            Node copy = new Node(cur.val);
+            map.put(cur, copy);
+            cur = cur.next;
+        }
+
+        cur = head;
+        while(cur != null){
+            Node copy = map.get(cur);
+            copy.next = map.get(cur.next);
+            copy.random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}
