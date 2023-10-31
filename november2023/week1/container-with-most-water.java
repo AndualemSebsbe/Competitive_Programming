0f4e5a0f067e12428e27497d4df056edccb4a720
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-10-31 04:02:55 +0300

    Added solution for Container With Most Water

---
+++ november2023/week1/container-with-most-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int len = height.length;
+        int maxValue = 0;
+        int minValue = 0;
+        int l=0, r = len - 1;
+        
+        while(l < r){
+            
+           minValue = Math.min(height[l], height[r]);
+           maxValue = Math.max(maxValue, minValue * (r-l));
+            if(minValue == height[l])
+                l++;
+            if(minValue == height[r])
+                r--;
+        }
+        return maxValue;
+    }
+}
