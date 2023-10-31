b7f2e57541d7e26e7a4737827d1d5e58dd991200
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-11-01 01:11:32 +0300

    Added solution for Palindrome Linked List

---
+++ november2023/week1/palindrome-linked-list.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode node;
+    public boolean isPalindrome(ListNode head) {
+        if(node == null) node = head;
+        if(head == null) return true;
+        
+        boolean flag = isPalindrome(head.next) && node.val == head.val;
+        node = node.next;
+        
+        return flag;
+    }
+}
