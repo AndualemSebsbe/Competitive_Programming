5568392c5dd34cfb83e83e0adce10357bc5a4420
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-12-01 22:45:05 +0300

    Added solution for Check If Two String Arrays are Equivalent

---
+++ november2023/week2/check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String res1 = "", res2 = "";
+        for(String s : word1)
+            res1 += s;
+        
+        for(String s : word2)
+            res2 += s;
+        
+        return res1.equals(res2);
+    }
+}
