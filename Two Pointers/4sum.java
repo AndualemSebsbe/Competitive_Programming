e08f8cf6a15408c94e1b69afe936f9b988101069
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-12-10 20:36:53 +0300

    Added solution for 4Sum

--- Two Pointers/4sum.java
+++ Two Pointers/4sum.java
@@ -1,38 +1,92 @@
-class Solution {
+// public class Solution {
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+//         Arrays.sort(nums); // Sort the input array in ascending order.
+//         List<List<Integer>> result = new ArrayList<>();
+//         List<Integer> temp = new ArrayList<>();
+//         helper(nums, (long) target, 0, result, temp, 4); // Use long data type for target.
+//         return result; // Return the result list containing unique quadruplets.
+//     }
+
+//     private void helper(int[] nums, long target, int start, List<List<Integer>> result, List<Integer> temp, int numNeed) {
+//         if (numNeed != 2) {
+//             for (int i = start; i < nums.length - numNeed + 1; i++) {
+//                 if (i > start && nums[i] == nums[i - 1]) {
+//                     continue; // Skip duplicates to avoid duplicate combinations.
+//                 }
+//                 temp.add(nums[i]); // Add the current number to the combination.
+//                 helper(nums, target - nums[i], i + 1, result, temp, numNeed - 1); // Recursively find the next number(s).
+//                 temp.remove(temp.size() - 1); // Remove the last number to backtrack.
+//             }
+//             return;
+//         }
+
+//         // If we need exactly 2 numbers, perform a two-pointer approach.
+//         int l = start;
+//         int r = nums.length - 1;
+//         while (l < r) {
+//             long total = (long) nums[l] + nums[r];
+//             if (total < target) {
+//                 l++;
+//             } else if (total > target) {
+//                 r--;
+//             } else {
+//                 temp.add(nums[l]); // Add the left number to the combination.
+//                 temp.add(nums[r]); // Add the right number to the combination.
+//                 result.add(new ArrayList<>(temp)); // Store the valid quadruplet in the result list.
+//                 temp.remove(temp.size() - 1); // Remove the right number to backtrack.
+//                 temp.remove(temp.size() - 1); // Remove the left number to backtrack.
+//                 l++;
+//                 r--;
+//                 while (l < r && nums[l] == nums[l - 1]) {
+//                     l++; // Skip duplicates on the left.
+//                 }
+//             }
+//         }
+//     }
+// }
+
+class Solution {  
+    List<List<Integer>> res = new ArrayList();  
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList();
-
-        for(int i = 0; i < n - 3; i++){
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
-            for(int j = i + 1; j < n - 2; j++){   
-                if(j > i + 1 && nums[j] == nums[j-1])
-                    continue;            
- 
-                int l = j + 1, r = n - 1; 
-                // to prevent integer overflow
-                long sum = nums[i] + nums[j];
-                long val = (long)target - sum;               
-                while(l < r){
-                    sum = nums[l] + nums[r];
-                    if(sum > val)
-                        r -= 1;
-                    else if(sum < val)
-                        l += 1;
-                    else{
-                        res.add(Arrays.asList(nums[i], nums[j], 
-                            nums[l], nums[r]));
-                            
-                        l += 1;
-                        while(l < r && nums[l] == nums[l-1])
-                            l += 1;
-                    }
-                }
-            }
-        }
+        
+        List<Integer> quad = new ArrayList();
+        kSum(4, 0, nums, (long)target, quad);
 
         return res;
+    }
+
+    void kSum(int k, int start, int[] nums, long target, List<Integer> quad){
+        if(k != 2){
+            for(int i = start; i < nums.length -  k + 1; i++){
+                if(i > start && nums[i] == nums[i - 1])
+                    continue;
+
+                quad.add(nums[i]);
+                kSum(k - 1, i + 1, nums, target - nums[i], quad);
+                quad.remove(quad.size() - 1);
+            }
+            return;
+        }
+
+        // base case, Two Sum II
+        int l = start, r = nums.length - 1;
+        while(l < r){
+            long sum = (long) nums[l] + nums[r];
+            if(sum > target)
+                r -= 1;
+            else if(sum < target)
+                l += 1;
+            else{
+                List<Integer> list = new ArrayList(quad);
+                list.add(nums[l]);
+                list.add(nums[r]);
+                res.add(new ArrayList(list));
+                l += 1;
+                while(l < r && nums[l] == nums[l - 1])
+                    l += 1;
+            }
+        }
     }
 }
