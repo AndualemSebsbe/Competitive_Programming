0c3cf8a141ef310f33391bb1cb20ea98b0777780
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-12-10 22:51:33 +0300

    Added solution for Transpose Matrix

---
+++ Matrix/transpose-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transMat = new int[n][m];
+
+        for(int col = 0; col < n; col++){
+            for(int row = 0; row < m; row++){
+                transMat[col][row] = matrix[row][col];
+            }
+        }
+
+        return transMat;
+    }
+}
