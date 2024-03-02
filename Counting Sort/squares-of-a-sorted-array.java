a83a8371a1273b5a0799981dcbbfdc1ec0a68047
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-03-03 01:59:29 +0300

    Added solution for Squares of a Sorted Array

---
+++ Counting Sort/squares-of-a-sorted-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] count = new int[10001];
+
+        for(int num : nums){
+            if(num < 0)
+                count[-1 * num] += 1;
+            else
+                count[num] += 1;
+        }
+
+        int idx = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i]-- > 0)
+                nums[idx++] = i * i;
+        }
+
+        return nums;
+    }
+}
