42895850bfcb337a461d3ad63b4421c781b6ed9c
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-03-23 03:31:05 +0300

    Added solution for Maximum Profit in Job Scheduling

--- Dynamic Programming I/maximum-profit-in-job-scheduling.java
+++ Dynamic Programming I/maximum-profit-in-job-scheduling.java
@@ -1,21 +1,44 @@
 class Solution {
-   public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    Integer[] dp = new Integer[50001];
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
         int[][] jobs = new int[n][3];
         for (int i = 0; i < n; i++)
             jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
         
-        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        for(int i = 0; i < n; i++)
+            startTime[i] = jobs[i][0];
+
+        return findMaxProfit(jobs, startTime, 0, n);
+    }
+
+    int findMaxProfit(int[][] jobs, int[] startTime, int idx, int n){
+        if(idx == n)
+            return 0;
         
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-        for (int[] job : jobs) {
-            int cur = dp.floorEntry(job[0]).getValue() + job[2];
-            
-            if (cur > dp.lastEntry().getValue())
-                dp.put(job[1], cur);
+        if(dp[idx] != null)
+            return dp[idx];
+        
+        int nextIdx = binarySearch(startTime, jobs[idx][1]);
+        
+        int pick = jobs[idx][2] + findMaxProfit(jobs, startTime, nextIdx, n);
+        int dontPick = findMaxProfit(jobs, startTime, idx + 1, n);
+        int max = Math.max(pick, dontPick);
+
+        return dp[idx] = max;
+    }
+
+    int binarySearch(int[] startTime, int endIdx){
+        int l = 0, r = startTime.length - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(startTime[mid] >= endIdx)
+                r = mid - 1;
+            else
+                l = mid + 1;
         }
 
-        return dp.lastEntry().getValue();
+        return l;
     }
 }
