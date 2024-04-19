5c800cf0bc16258e0c7d3b5f75677db951198ee6
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-04-19 21:26:16 +0300

    Added solution for Number of Islands

---
+++ Depth First Search/number-of-islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length, count = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1')
+                    count += dfs(grid, i, j);
+            }
+        }
+        return count;
+    }
+    
+    public int dfs(char[][] grid, int row, int col){
+        if(row < 0 || row >= grid.length || col < 0 || 
+        col >= grid[0].length || grid[row][col] == '0')
+            return 0;
+        grid[row][col] = '0';
+        
+        int count = 1;
+        int[][] dirs = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
+        for(int[] dir : dirs){
+          int newR = row + dir[0];
+          int newC = col + dir[1];
+          dfs(grid, newR, newC);
+        }
+        
+        return count;
+    }
+}
