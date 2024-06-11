b22e069cf87cc94e9f12167786220af12ab1dbd4
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-06-11 18:13:16 +0300

    Added solution for Relative Sort Array

--- Arrays and Hashing/relative-sort-array.java
+++ Arrays and Hashing/relative-sort-array.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Set<Integer> set = new HashSet();
         int n = arr1.length;
+        Map<Integer, List<Integer>> map = new HashMap();
+        for(int i = 0; i < n; i++){
+            if(!map.containsKey(arr1[i]))
+                map.put(arr1[i], new ArrayList());
+            map.get(arr1[i]).add(i);
+        }
+
         int[] res = new int[n];
         int idx = 0;
+        Set<Integer> set = new HashSet();
         for(int num : arr2){
-            for(int i = 0; i < n; i++){
-                if(arr1[i] == num)
-                    res[idx++] = num;
-            }
+            for(int i : map.get(num))
+                res[idx++] = arr1[i];
             set.add(num);
         }
 
