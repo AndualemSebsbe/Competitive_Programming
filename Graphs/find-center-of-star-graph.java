4009a7efdf88baabc7465bfd8b19d4ae58bed850
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-06-29 01:28:14 +0300

    Added solution for Find Center of Star Graph

--- Graphs/find-center-of-star-graph.java
+++ Graphs/find-center-of-star-graph.java
@@ -1,19 +1,16 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int n = edges.length;
         int[] degree = new int[100001];
-        Set<Integer> set = new HashSet();
         for(int[] edge : edges){
             int src = edge[0], dst = edge[1];
             degree[src] += 1;
             degree[dst] += 1;
-            set.add(src);
-            set.add(dst);
         }
 
-        int count = set.size();
+        // number of nodes
+        int n = edges.length + 1;
         for(int i = 0; i < degree.length; i++){
-            if(degree[i] == count - 1)
+            if(degree[i] == n - 1)
                 return i;
         }
 
