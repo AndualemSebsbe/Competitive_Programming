94b9fa15430976914f6a3a43746d91f264db1b5e
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-07-30 04:52:07 +0300

    Added solution for Count Number of Teams

--- Dynamic Programming II/count-number-of-teams.java
+++ Dynamic Programming II/count-number-of-teams.java
@@ -1,34 +1,32 @@
 class Solution {
     public int numTeams(int[] rating) {
         int n = rating.length;
-        Integer[][][] memo = new Integer[n][4][2];
+        int[][] ascend = new int[n][4];
+        int[][] descend = new int[n][4];
+
+        for(int i = 0; i < n; i++){
+            ascend[i][1] = 1;
+            descend[i][1] = 1;
+        }
+
+        for(int count = 2; count < 4; count++){
+            for(int i = 0; i < n; i++){
+                for(int j = i + 1; j < n; j++){
+                    if(rating[i] < rating[j])
+                        ascend[i][count] += ascend[j][count - 1];
+                    
+                    if(rating[i] > rating[j])
+                        descend[i][count] += descend[j][count - 1];
+                }
+            }
+        }
+
         int teams = 0;
         for(int i = 0; i < n; i++){
-            teams += countTeams(rating, i, 1, 1, memo);
-            teams += countTeams(rating, i, 1, 0, memo);
+            teams += ascend[i][3];
+            teams += descend[i][3];
         }
 
         return teams;
-    }
-
-    int countTeams(int[] rating, int i, int count, int ascend, Integer[][][] memo){
-        if(count == 3)
-            return 1;
-        if(i == rating.length)
-            return 0;
-
-        if(memo[i][count][ascend] != null)
-            return memo[i][count][ascend];
-        
-        int teams = 0; 
-        for(int j = i + 1; j < rating.length; j++){
-            if(ascend == 1 && rating[i] < rating[j])
-                teams += countTeams(rating, j, count + 1, ascend, memo);
-            
-            if(ascend == 0 && rating[i] > rating[j])
-                teams += countTeams(rating, j, count + 1, ascend, memo);
-        }
-
-        return memo[i][count][ascend] = teams;
     }
 }
