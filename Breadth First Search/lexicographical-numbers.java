d673bf345f7b4a7a66bc3efc6f6f3ac591db5bd1
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-09-22 02:49:29 +0300

    Added solution for Lexicographical Numbers

--- Breadth First Search/lexicographical-numbers.java
+++ Breadth First Search/lexicographical-numbers.java
@@ -2,24 +2,22 @@ class Solution {
     Map<Integer, List<Integer>> graph = new HashMap();
     public List<Integer> lexicalOrder(int n) {
         for (int i = 1; i <= n; i++) {
-            int par = i / 10;
-            if (par != 0) {
-                if (!graph.containsKey(par))
-                    graph.put(par, new ArrayList());
-                graph.get(par).add(i);
-            }
+            int par = i / 10;            
+            if (!graph.containsKey(par))
+                graph.put(par, new ArrayList());
+            graph.get(par).add(i);
         }
 
         List<Integer> res = new ArrayList();
-        for (int i = 1; i <= n && i <= 9; i++) {
-            dfs(res, i);
-        }
+        dfs(res, 0);
 
         return res;
     }
 
     void dfs(List<Integer> res, int num) {
-        res.add(num);
+        if (num != 0)
+            res.add(num);
+            
         if (graph.containsKey(num)) {
             for (int child : graph.get(num)) {
                 dfs(res, child);
