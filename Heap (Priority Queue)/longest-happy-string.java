477a8908d2c2c7552a810cdca777589734a5da20
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-10-17 21:24:44 +0300

    Added solution for Longest Happy String

--- Heap (Priority Queue)/longest-happy-string.java
+++ Heap (Priority Queue)/longest-happy-string.java
@@ -8,34 +8,35 @@ class Solution {
         if (c > 0) 
             pq.offer(new int[]{c, 'c'});
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder res = new StringBuilder();
 
         while (!pq.isEmpty()) {
             int[] first = pq.poll();
 
             // Check if last two characters are the same.
-            if (result.length() >= 2 && result.charAt(result.length() - 1) == first[1] &&
-                result.charAt(result.length() - 2) == first[1]) {
+            int len = res.length();
+            if (len >= 2 && res.charAt(len - 1) == first[1] &&
+                res.charAt(len - 2) == first[1]) {
 
                 if (pq.isEmpty()) 
                     break;  // No more valid characters.
 
                 // Pick the second character.
                 int[] second = pq.poll();
-                result.append((char) second[1]);
+                res.append((char) second[1]);
                 second[0] -= 1;
 
-                if (second[0] > 0) pq.offer(second);
-                pq.offer(first);
+                if (second[0] > 0) 
+                    pq.offer(second);
             } else {
-                result.append((char) first[1]);
+                res.append((char) first[1]);
                 first[0] -= 1;
-
-                if (first[0] > 0) 
-                    pq.offer(first);
             }
+            
+            if (first[0] > 0) 
+                pq.offer(first);
         }
 
-        return result.toString();
+        return res.toString();
     }
 }
