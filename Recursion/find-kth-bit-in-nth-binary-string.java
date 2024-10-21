9de789ca4b4c0fe724fd251e91b209e5dd73af7e
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-10-21 04:53:19 +0300

    Added solution for Find Kth Bit in Nth Binary String

--- Recursion/find-kth-bit-in-nth-binary-string.java
+++ Recursion/find-kth-bit-in-nth-binary-string.java
@@ -1,18 +1,20 @@
 class Solution {
     public char findKthBit(int n, int k) {
+        return helper(n, k, false);
+    }
+
+    char helper(int n, int k, boolean invert) {
         if (n == 1)
-            return '0';
+            return !invert ? '0' : '1';
 
         int len = (int) Math.pow(2, n) - 1;
         int mid = len / 2;
 
         if (k <= mid)
-            return findKthBit(n - 1, k);
+            return helper(n - 1, k, invert);
         else if (k > mid + 1) {
-            char res = findKthBit(n - 1, 1 + len - k);
-            return res == '0' ? '1' : '0';
-        }
-        else 
-            return '1';
+            return helper(n - 1, 1 + len - k, !invert);
+        } else
+            return !invert ? '1' : '0';
     }
 }
