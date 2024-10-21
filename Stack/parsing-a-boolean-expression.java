c722fbcc789da70fee93e19168e0be4c501eb580
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-10-21 03:16:26 +0300

    Added solution for Parsing A Boolean Expression

--- Stack/parsing-a-boolean-expression.java
+++ Stack/parsing-a-boolean-expression.java
@@ -1,52 +1,38 @@
 class Solution {
-
     public boolean parseBoolExpr(String expression) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> op = new Stack();
+        Stack<Character> st = new Stack();
+        for (char c : expression.toCharArray()) {
 
-        // Traverse the entire expression
-        for (char currChar : expression.toCharArray()) {
-            if (currChar == ')') {
-                ArrayList<Character> values = new ArrayList<>();
-
-                // Gather all values inside the parentheses
-                while (st.peek() != '(') {
-                    values.add(st.pop());
+            if (c == '&' || c == '|' || c == '!')
+                op.add(c);
+            else if (c == ')') {
+                boolean res = st.pop() == 't';
+                while (st.peek() == ',') {
+                    st.pop();
+                    boolean val = st.pop() == 't';
+                    if (op.peek() == '&')
+                        res = res && val;
+                    else 
+                        res = res || val;
                 }
-                st.pop(); // Remove '('
-                char op = st.pop(); // Remove the operator
 
-                // Evaluate the subexpression and push the result back
-                char result = evaluateSubExpr(op, values);
-                st.push(result);
-            } else if (currChar != ',') {
-                st.push(currChar); // Push non-comma characters into the stack
+                if (st.peek() == '(')
+                    st.pop();
+                if (op.peek() == '!')
+                    res = !res;
+                
+                if (res)
+                    st.add('t');
+                else
+                    st.add('f');
+
+                op.pop();
             }
+            else
+                st.add(c);
         }
 
-        // Final result is on the top of the stack
-        return st.peek() == 't';
-    }
-
-    // Evaluates a subexpression based on the operator and list of values
-    private char evaluateSubExpr(char op, ArrayList<Character> values) {
-        if (op == '!') return values.get(0) == 't' ? 'f' : 't';
-
-        // AND: return 'f' if any value is 'f', otherwise return 't'
-        if (op == '&') {
-            for (char value : values) {
-                if (value == 'f') return 'f';
-            }
-            return 't';
-        }
-
-        // OR: return 't' if any value is 't', otherwise return 'f'
-        if (op == '|') {
-            for (char value : values) {
-                if (value == 't') return 't';
-            }
-            return 'f';
-        }
-
-        return 'f'; // This point should never be reached
+        return st.pop() == 't';
     }
 }
