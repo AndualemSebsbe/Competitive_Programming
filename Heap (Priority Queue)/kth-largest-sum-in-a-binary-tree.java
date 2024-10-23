ae5f31cb4ff28154b9a9b4b29310b7b054552590
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-10-23 03:18:02 +0300

    Added solution for Kth Largest Sum in a Binary Tree

--- Heap (Priority Queue)/kth-largest-sum-in-a-binary-tree.java
+++ Heap (Priority Queue)/kth-largest-sum-in-a-binary-tree.java
@@ -1,32 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public long kthLargestLevelSum(TreeNode root, int k) {
-        // max heap
-        PriorityQueue<Long> pq = new PriorityQueue<>(
-                Collections.reverseOrder());
-
-        Queue<TreeNode> bfsQueue = new LinkedList<>();
-        bfsQueue.add(root);
-        while (!bfsQueue.isEmpty()) {
-            int size = bfsQueue.size();
-            long sum = 0;
-            for (int i = 0; i < size; i++) {
-                TreeNode poppedNode = bfsQueue.remove();
-                sum += poppedNode.val;
-
-                if (poppedNode.left != null)
-                    bfsQueue.add(poppedNode.left);
-
-                if (poppedNode.right != null)
-                    bfsQueue.add(poppedNode.right);
-            }
-            pq.add(sum);
-        }
+        PriorityQueue<Long> maxHeap = new PriorityQueue<>((a, b) -> Long.compare(b, a));
         
-        if (pq.size() < k)
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long sum = 0;
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                sum += cur.val;
+
+                if (cur.left != null)
+                    queue.add(cur.left);
+                if (cur.right != null)
+                    queue.add(cur.right);
+            }
+
+            maxHeap.add(sum);
+        }
+
+        if (maxHeap.size() < k)
             return -1;
-        for (int i = 0; i < k - 1; i++)
-            pq.remove();
-        return pq.peek();
+        while (k-- > 1)
+            maxHeap.poll();
+        return maxHeap.peek();
     }
 }
