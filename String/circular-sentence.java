80a0a845ba113df5232e0c3b93271c40eae455e9
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-11-03 02:33:03 +0300

    Added solution for Circular Sentence

---
+++ String/circular-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        int n = words.length;
+        char lastChar = words[0].charAt(words[0].length() - 1);
+
+        for (int i = 1; i < n; i++) {
+            int len = words[i].length();
+            char firstChar = words[i].charAt(0);
+            System.out.println(firstChar + " " + lastChar);
+            if (firstChar != lastChar)
+                return false;
+            
+            lastChar = words[i].charAt(len - 1);
+        }
+
+        return words[0].charAt(0) == lastChar;
+    }
+}
