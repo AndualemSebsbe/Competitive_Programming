9859dd6c289ad000e3510ef319b375a069dc8944
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-11-07 02:59:36 +0300

    Added solution for Find if Array Can Be Sorted

--- Bit Manipulation/find-if-array-can-be-sorted.java
+++ Bit Manipulation/find-if-array-can-be-sorted.java
@@ -4,28 +4,28 @@ class Solution {
         int lastGroupHigh = 0;
         boolean bitChange;
 
-        for(int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++) {
             bitChange = bitCounts(high) != bitCounts(nums[i]);
 
-            if(bitChange)
+            if (bitChange)
                 lastGroupHigh = high;
-            
-            if(nums[i] > high)
+
+            if (nums[i] > high)
                 high = nums[i];
-            
-            if(nums[i] < lastGroupHigh)
+
+            if (nums[i] < lastGroupHigh)
                 return false;
         }
 
         return true;
-    } 
+    }
 
-    int bitCounts(int num){
+    int bitCounts(int num) {
         int count = 0;
-        while(num > 0){
-            if((num & 1) == 1)
+        while (num > 0) {
+            if ((num & 1) == 1)
                 count += 1;
-            
+
             num = num >> 1;
         }
 
