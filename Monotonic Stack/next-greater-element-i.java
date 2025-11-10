d1a0ecf54b0b1e19c6689179d9e22e3df4ea0306
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2025-11-10 11:42:04 +0300

    Added solution for Next Greater Element I

--- Monotonic Stack/next-greater-element-i.java
+++ Monotonic Stack/next-greater-element-i.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int len1 = nums1.length, len2 = nums2.length;
-        int max = 0;
-        for (int num : nums2) 
-            max = Math.max(max, num);
-
-        int[] nextGreater = new int[max + 1];
-        Arrays.fill(nextGreater, -1);
-        
+        Map<Integer, Integer> map = new HashMap<>();        
         Stack<Integer> st = new Stack<>();
         for (int num : nums2) {
             while (!st.isEmpty() && num > st.peek()) {
-                nextGreater[st.peek()] = num;
+                map.put(st.peek(), num);
                 st.pop();
             }
 
             st.add(num);
         }
 
-        int[] res = new int[len1];
-        for (int i = 0; i < len1; i++) 
-            res[i] = nextGreater[nums1[i]];
+        for (int i = 0; i < len1; i++) {
+            int num = nums1[i];
+            if (map.containsKey(num))
+                nums1[i] = map.get(num);
+            else 
+                nums1[i] = -1;
+        }
         
-        return res;
+        return nums1;
     }
 }
